b6d4f1081f5583f70e95de5140bb745608451ef8
Adnan Omar <adnanadil377@gmail.com>
2024-05-27 12:51:30 +0300

    change the layout in card form

--- index.php
+++ index.php
@@ -10,12 +10,45 @@
     <div class="container">
         <br>
         <img src="./logob.png" style="width: 10rem;"><br><br>
-        <button type="button" class="btn btn-outline-primary btn-lg" onclick="window.location.href = 'inventory.php'">Inventory</button><br><br>
-        <button type="button" class="btn btn-outline-primary btn-lg" onclick="window.location.href = 'purchase.php'">Purchase</button><br><br>
-        <button type="button" class="btn btn-outline-primary btn-lg" onclick="window.location.href = 'sell.php'">Sell</button><br><br>
-        <button type="button" class="btn btn-outline-primary btn-lg" onclick="window.location.href = 'supplier.php'">Supplier</button><br><br>
-        <button type="button" class="btn btn-outline-primary btn-lg" onclick="window.location.href = 'customer.php'">Customer</button><br><br>
-        <button type="button" class="btn btn-outline-primary btn-lg" onclick="window.location.href = 'invoice.php'">Invoice</button><br><br>
+        <div class="row gap-5">
+            <div class="card shadow p-3 border-0" style="width: 18rem;" onclick="window.location.href = 'inventory.php'"><br><br>
+                <center><h1 style="font-size: 70px;">📦  💰</h1></center>
+                <center><h1 style="font-size: 70px;">📈</h1></center>
+                <div class="card-body"><br>
+                    <center><h5 class="card-title">Inventory</h5></center>
+                </div>
+            </div>
+            <div class="card shadow p-3 border-0" style="width: 18rem;" onclick="window.location.href = 'purchase.php'"><br><br><br>
+                <center><h1 style="font-size: 100px;">🛒</h1></center>
+                <div class="card-body"><br>
+                    <center><h5 class="card-title">Purchase</h5></center>
+                </div>
+            </div>
+            <div class="card shadow p-3 border-0" style="width: 18rem;" onclick="window.location.href = 'sell.php'"><br><br><br>
+                <center><h1 style="font-size: 100px;">🏷️</h1></center>
+                <div class="card-body"><br>
+                    <center><h5 class="card-title">Sale</h5></center>
+                </div>
+            </div>
+            <div class="card shadow p-3 border-0" style="width: 18rem;" onclick="window.location.href = 'supplier.php'"><br><br><br>
+                <center><h1 style="font-size: 100px;">🚛</h1></center>
+                <div class="card-body"><br>
+                    <center><h5 class="card-title">Supplier</h5></center>
+                </div>
+            </div>
+            <div class="card shadow p-3 border-0" style="width: 18rem;" onclick="window.location.href = 'customer.php'"><br><br><br>
+                <center><h1 style="font-size: 100px;">👨🏻</h1></center>
+                <div class="card-body"><br>
+                    <center><h5 class="card-title">Customer</h5></center>
+                </div>
+            </div>
+            <div class="card shadow p-3 border-0" style="width: 18rem;" onclick="window.location.href = 'sell.php'"><br><br><br>
+                <center><h1 style="font-size: 100px;">📋</h1></center>
+                <div class="card-body"><br>
+                    <center><h5 class="card-title">Sale</h5></center>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
