5463ab25e0f64380f89c103e519ceee13a3c8bbd
Adnan Omar <adnanadil377@gmail.com>
2024-06-05 10:09:51 +0300

    css changes

--- supplier.php
+++ supplier.php
@@ -10,9 +10,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
     <title>Supplier</title>
+    <style>
+
+        @media (min-width:1000px) {
+            
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f4f4f4;
+                margin: 5vh;
+                padding: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            .ff{
+                background-color: white;
+                padding: 20px;
+                border-radius: 10px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+        }
+    </style>
 </head>
 <body>
-<div class="container">
+<div class="container ff">
     <br>
     <button class="btn btn-outline-secondary" onclick="window.location.href='./index.php'"><</button>
     <br>
@@ -132,7 +153,7 @@
             // Display student information in a table
             echo '<table class="table table-hover">';
             echo '<thead>';
-            echo '<tr>';
+            echo '<tr class="table-light">';
             echo '<th>Supplier Id</th>';
             echo '<th>Supplier Name</th>';
             echo '<th>Origin</th>';
