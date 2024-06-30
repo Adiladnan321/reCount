a543742018d19d7947bb71427346d6e77d35c6cd
Adnan Omar <adnanadil377@gmail.com>
2024-06-30 14:54:21 +0300

    changes to paths

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
         ?>
     </form><br>
         <div class="row gap-5">
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'inventory.php'"><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'inventory'"><br><br>
                 <center><h1 style="font-size: 70px;">📦  💰</h1></center>
                 <center><h1 style="font-size: 70px;">📈</h1></center>
                 <div class="card-body"><br>
@@ -92,49 +92,49 @@
                         ?></h5></center>
                 </div>
             </div>
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'purchase.php'"><br><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'purchase'"><br><br><br>
                 <center><h1 style="font-size: 100px;">🛒</h1></center>
                 <div class="card-body"><br>
                     <center><h5 class="card-title">Purchase</h5></center>
                 </div>
             </div>
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'sell.php'"><br><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'sell'"><br><br><br>
                 <center><h1 style="font-size: 100px;">🏷️</h1></center>
                 <div class="card-body"><br>
                     <center><h5 class="card-title">Sale</h5></center>
                 </div>
             </div>
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'supplier.php'"><br><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'supplier'"><br><br><br>
                 <center><h1 style="font-size: 100px;">🚛</h1></center>
                 <div class="card-body"><br>
                     <center><h5 class="card-title">Supplier</h5></center>
                 </div>
             </div>
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'customer.php'"><br><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'customer'"><br><br><br>
                 <center><h1 style="font-size: 100px;">👨🏻</h1></center>
                 <div class="card-body"><br>
                     <center><h5 class="card-title">Customer</h5></center>
                 </div>
             </div>
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'invoice.php'"><br><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'invoice'"><br><br><br>
                 <center><h1 style="font-size: 100px;">📋</h1></center>
                 <div class="card-body"><br>
                     <center><h5 class="card-title">Invoice</h5></center>
                 </div>
             </div>
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'cardflow.php'"><br><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'cardflow'"><br><br><br>
                 <center><h1 style="font-size: 100px;">💸</h1></center>
                 <div class="card-body"><br>
                     <center><h5 class="card-title">Cash Flow</h5></center>
                 </div>
             </div>
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'credit.php'"><br><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'credit'"><br><br><br>
                 <center><h1 style="font-size: 100px;">💳</h1></center>
                 <div class="card-body"><br>
                     <center><h5 class="card-title">Credit</h5></center>
                 </div>
             </div>
-            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'reports.php'"><br><br><br>
+            <div class="card shadow p-3 border-0 mobile-card" style="width: 18rem;" onclick="window.location.href = 'reports'"><br><br><br>
                 <center><h1 style="font-size: 100px;">📑</h1></center>
                 <div class="card-body"><br>
                     <center><h5 class="card-title">Reports</h5></center>
